5a0c1c0a4e71c73e2683c97845f5f7d7514f24ff
snehaborole5-ui <snehaborole5@gmail.com>
2026-04-26 14:52:56 +0530

    sidebar task new

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,15 @@
-const sidebar = document.getElementById("sidebar");
-const sideBarClose = document.getElementById("sideBarClose");
+const sidebar     =document.getElementById("sidebar"); 
+const sideBarClose= document.getElementById("sideBarClose"); 
 const sideBarOpen = document.getElementById("sideBarOpen");
-const overlay = document.getElementById("overlay");
+const overlay =document.getElementById("overlay");
+
+function toggleSideBar(){ 
+ sidebar.classList.toggle('active'); 
+ overlay.classList.toggle('active');
+     
+}
 
 
-// OPEN
-sideBarOpen.addEventListener("click", () => {
-  sidebar.classList.add("active");
-  overlay.classList.add("active");
-});
-
-// CLOSE by X
-sideBarClose.addEventListener("click", () => {
-  sidebar.classList.remove("active");
-  overlay.classList.remove("active");
-});
-
-// CLOSE by clicking overlay
-overlay.addEventListener("click", () => {
-  sidebar.classList.remove("active");
-  overlay.classList.remove("active");
-});
+sideBarOpen.addEventListener('click', toggleSideBar) ;
+sideBarClose.addEventListener('click', toggleSideBar); 
+overlay.addEventListener('click',toggleSideBar);
